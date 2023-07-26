2074b973f8110d96c56b2897d7d84a8e983bf2de
shgus1224 <shgus1224@naver.com>
2023-07-26 11:55:02 +0900

    fix: header 가로 및 세로 중간정렬

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 
 export const MainContainer = styled.header`
   position: fixed;
+  display: flex;
+  justify-content: center;
   width: 100%;
   height: 60px;
   border-bottom: 2px solid #313131;
@@ -9,12 +11,15 @@ export const MainContainer = styled.header`
   z-index: auto;
 `;
 
-export const HeaderList = styled.div``;
+export const HeaderList = styled.div`
+  font-weight: bold;
+`;
 
 export const HeaderWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  gap: 20px;
+
+  gap: 30px;
 `;
