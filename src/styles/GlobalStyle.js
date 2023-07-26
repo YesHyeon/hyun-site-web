ccc6fbb423bb5b8a65918b6b2222bb363129fba9
shgus1224 <shgus1224@naver.com>
2023-07-26 11:52:59 +0900

    feat: 전역 스타일 적용

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,36 @@
+import { Global, css } from '@emotion/react';
+import './font.css';
+
+const defaultStyle = css`
+  * {
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Heebo', sans-serif;
+    font-display: fallback;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    outline: none;
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    background: none;
+  }
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={defaultStyle} />;
+};
+
+export default GlobalStyle;
