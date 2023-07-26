277277e13f9369fc347828fc60da6133d52310e4
shgus1224 <shgus1224@naver.com>
2023-07-26 14:27:29 +0900

    feat: 글자 타이핑 애니메이션 추가

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,14 +1,63 @@
+import { gsap } from 'gsap';
 import './index.css';
-import { Link } from 'react-scroll';
-import profileImage from '../../images/introduce/hyun-profile.png';
-import { MainContainer } from './index.styles';
+import { MainContainer, Title, Description } from './index.styles';
+import { useState, useEffect, useRef } from 'react';
 
 function MainPage() {
+  const NAME_TEXT = 'Hyun Roh';
+
+  const DES_TEXT = [
+    '기록과 디자인을 즐기는',
+    'React를 사용하는',
+    'Typescript를 사용하는',
+    '새로움에 도전적인',
+  ];
+
+  const [text, setText] = useState('');
+  const [descriptionText, setDescritionText] = useState('');
+
+  const [textCount, setTextCount] = useState(0);
+  const [descriptionCount, setDescriptionCount] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    const nameInterval = setInterval(() => {
+      if (textCount !== NAME_TEXT.length) {
+        setText(text + NAME_TEXT[textCount]);
+        setTextCount(textCount + 1);
+      }
+
+      // 글자 다 안써졌을때
+      if (descriptionCount < DES_TEXT[index].length) {
+        setDescritionText(descriptionText + DES_TEXT[index][descriptionCount]);
+        setDescriptionCount(descriptionCount + 1);
+      }
+      // 글자 다 써졌을때, 다시 지우는 과정
+      else {
+        setDescritionText(descriptionText.slice(0, -1));
+
+        if (descriptionText.length === 1) {
+          setDescriptionCount(0);
+          if (index === DES_TEXT.length - 1) {
+            setIndex(0);
+          } else {
+            setIndex(index + 1);
+          }
+        }
+      }
+    }, 100);
+
+    return () => clearInterval(nameInterval);
+  });
+
   return (
     <MainContainer>
-      <div id="main">
-        <div className="title">Hyun</div>
-      </div>
+      <Title>{text}</Title>
+      <Description ref={boxRef}>안녕하세요</Description>
+      <Description ref={boxRef}>{descriptionText}</Description>
+      <Description ref={boxRef}>프론트엔드 개발자 노현입니다.</Description>
     </MainContainer>
   );
 }
--- src/components/main/index.styles.js
+++ src/components/main/index.styles.js
@@ -4,6 +4,22 @@ import roomStartBack from '../../images/banners/background1.png';
 export const MainContainer = styled.div`
   background-image: url(${roomStartBack});
   background-size: cover;
+  width: 100vw;
   height: 100vh;
   border-bottom: 3px solid black;
+  overflow-x: hidden;
 `;
+
+export const Title = styled.div`
+  font-family: 'Babylonica', cursive;
+  font-size: 200px;
+  color: #fff;
+`;
+
+export const Description = styled.div`
+  font-family: 'Babylonica', cursive;
+  font-size: 50px;
+  color: #fff;
+`;
+
+export const Main = styled.div``;
