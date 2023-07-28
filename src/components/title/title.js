39053d8414e41c4028b72b4fd321569afaaa02a7
shgus1224 <shgus1224@naver.com>
2023-07-28 21:57:38 +0900

    feat: 인덱스 제목코드 컴포넌트화

---
+++ src/components/title/title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MainContainer } from './title.styles';
+
+const title = () => {
+  return <MainContainer>About Me</MainContainer>;
+};
+
+export default title;
---
+++ src/components/title/title.styles.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.header`
+  font-size: 100px;
+  font-family: 'BlackHanSans';
+  color: white;
+  position: absolute;
+  top: 30px;
+  left: 40px;
+
+  z-index: -100px;
+`;
