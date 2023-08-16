4a3a05b16d92455c86dce8b0f0f93ea4be93a49c
shgus1224 <shgus1224@naver.com>
2023-08-16 18:50:45 +0900

    style: 타이틀 네온 스타일로 수정

--- src/components/title/title.styles.js
+++ src/components/title/title.styles.js
@@ -1,12 +1,39 @@
 import styled from '@emotion/styled';
 
 export const MainContainer = styled.header`
-  font-size: 100px;
-  font-family: 'BlackHanSans';
-  color: black;
-  position: absolute;
-  top: 30px;
-  left: 40px;
+  position: sticky;
+  text-align: center;
+  top: 0px;
+  font-size: 150px;
+  font-family: 'MontserratBold';
+  margin: 0 auto;
+  background-color: black;
+  height: 150px;
 
-  z-index: -100px;
+  font-size: 100px;
+  color: #fff;
+  animation: neonGlow 2s infinite alternate;
+
+  &:before {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #000;
+    z-index: -1;
+  }
+
+  @keyframes neonGlow {
+    0% {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #ff00e5,
+        0 0 40px #ff00e5, 0 0 50px #ff00e5, 0 0 60px #ff00e5, 0 0 70px #ff00e5;
+    }
+    20% {
+      text-shadow: none;
+    }
+    100% {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #ff00e5,
+        0 0 40px #ff00e5, 0 0 50px #ff00e5, 0 0 60px #ff00e5, 0 0 70px #ff00e5;
+    }
+  }
 `;
