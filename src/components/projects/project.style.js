3573ed71ea7f53fe1218e902e1e07c878d45d4db
shgus1224 <shgus1224@naver.com>
2023-08-25 18:29:11 +0900

    feat: 프로젝트 컴포넌트 추가

--- src/components/projects/project.style.js
+++ src/components/projects/project.style.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { floatAnimation } from '../../utils/floatingAnimation';
 
 export const MainContainer = styled.div`
   display: flex;
@@ -7,27 +8,93 @@ export const MainContainer = styled.div`
   width: 100%;
   height: 100%;
   margin-top: 100px;
-
-  background-color: green;
 `;
 
 export const Header = styled.div`
-  width: 100px;
+  width: 100vw;
   margin: 0 auto;
   font-family: BlackHanSans;
-  font-size: 30px;
+  font-size: 2.5rem;
+
+  display: flex;
+  flex: 0 0 auto;
+  white-space: nowrap;
+  overflow: hidden;
+  transition: 0.3s;
+  font-size: 15rem;
+  font-weight: bold;
+
+  .loop-text {
+    padding-right: 10vw;
+    animation: textLoop 20s linear infinite;
+
+    @keyframes textLoop {
+      0% {
+        -webkit-transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0);
+      }
+      100% {
+        -webkit-transform: translate3d(-100%, 0, 0);
+        transform: translate3d(-100%, 0, 0);
+      }
+    }
+  }
 `;
 
 export const CardContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 90vw;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 60vw;
   align-items: center;
-  padding-inline: 100px;
   margin: 0 auto;
   gap: 30px;
-  background-color: white;
+  flex-wrap: wrap;
+`;
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 300px;
+  margin: 0 auto;
   border-radius: 30px;
+  background-color: #ededed;
+
+  animation-fill-mode: forwards;
+  animation-name: cardSlideRightAnimation, ${floatAnimation};
+  animation-delay: 0s, 1s;
+  animation-duration: 1s, 6s;
+  animation-timing-function: linear, ease-in-out;
+  animation-iteration-count: 1, infinite;
+
+  img {
+    width: 100%;
+    height: 200px;
+    border-top-right-radius: 30px;
+    border-top-left-radius: 30px;
+    margin-bottom: 30px;
+  }
+
+  .kr {
+    padding-left: 10px;
+    font-family: 'NanumSquareR';
+    font-weight: bold;
+  }
+
+  .title {
+    padding-left: 10px;
+    font-family: 'MontserratBold';
+    color: #bebcbc;
+  }
+
+  .date {
+    padding-right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    font-family: 'MontserratThin';
+    font-weight: bold;
+  }
 `;
 
 export const Card = styled.div`
@@ -43,10 +110,10 @@ export const CardDescriptionContainer = styled.div`
 `;
 
 export const CardText = styled.div`
-  height: 300px;
   background-color: red;
   font-family: 'NanumSquareR';
   font-size: 20px;
+  color: black;
 `;
 
 export const CardLinkContainer = styled.div`
