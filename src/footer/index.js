98ec4a76af8bf9f495ce8f10b6e59518284da77b
shgus1224 <shgus1224@naver.com>
2023-08-27 02:09:58 +0900

    feat: footer 추가

---
+++ src/footer/index.js
@@ -0,0 +1,7 @@
+import { MainContainer } from './index.styles';
+
+const Footer = () => {
+  return <MainContainer>Copyright © 2023 Hyun Roh</MainContainer>;
+};
+
+export default Footer;
---
+++ src/footer/index.styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  border-top: 1px solid white;
+  margin-inline: 30px;
+`;
